2dbfb8dec075b30a6af4c385f3f2478437743d3e
Areej <Areej-fahad>
2022-10-30 05:49:44 +0300

    Hw2

---
+++ hw2/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Nav from "./component/Nav";
+import Cards from "./component/Cards";
+import Footer from "./component/Footer"
+
+function App() {
+  return (
+    <>
+    <Nav></Nav>
+    <Cards></Cards>
+    <Footer></Footer>
+    
+
+    </>
+  );
+}
+
+export default App;
